4fc6a623b40fdfdf0c08d87928a37ac665a2b5d1
teguhpermadi <tguh.permadi@gmail.com>
2021-06-09 12:32:32 +0700

    custom view register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,144 @@
-<x-guest-layout>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Register &mdash; Stisla</title>
+
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    <!-- CSS Libraries -->
+    {{-- <link rel="stylesheet" href="../node_modules/bootstrap-social/bootstrap-social.css"> --}}
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ url('stisla/css/style.css') }}">
+    <link rel="stylesheet" href="{{ url('stisla/css/components.css') }}">
+</head>
+
+<body>
+    <div id="app">
+        <section class="section">
+            <div class="d-flex flex-wrap align-items-stretch">
+                <div class="col-lg-4 col-md-6 col-12 order-lg-1 min-vh-100 order-2 bg-white">
+                    <div class="p-4 m-3">
+                        <img src="{{ url('stisla/img/stisla-fill.svg') }}" alt="logo" width="80"
+                            class="shadow-light rounded-circle mb-5 mt-2">
+                        <h4 class="text-dark font-weight-normal">Welcome to <span class="font-weight-bold">Stisla</span>
+                        </h4>
+                        <p class="text-muted">Before you get started, you must register if you don't already
+                            have an account.</p>
+
+                        <form method="POST" action="{{ route('register') }}" class="needs-validation" novalidate="">
+                            @csrf
+
+                            <div class="form-group">
+                                <x-jet-label for="name" value="{{ __('Name') }}" />
+                                <x-jet-input id="name" class="form-control" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                            </div>
+                
+                            <div class="form-group">
+                                <x-jet-label for="email" value="{{ __('Email') }}" />
+                                <x-jet-input id="email" class="form-control" type="email" name="email" :value="old('email')" required />
+                            </div>
+                
+                            <div class="form-group">
+                                <x-jet-label for="password" value="{{ __('Password') }}" />
+                                <x-jet-input id="password" class="form-control" type="password" name="password" required autocomplete="new-password" />
+                            </div>
+                
+                            <div class="form-group">
+                                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                                <x-jet-input id="password_confirmation" class="form-control" type="password" name="password_confirmation" required autocomplete="new-password" />
+                            </div>
+                
+                            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
+                                <div class="form-group">
+                                    <x-jet-label for="terms">
+                                        <div class="flex items-center">
+                                            <x-jet-checkbox name="terms" id="terms"/>
+                
+                                            <div class="ml-2">
+                                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
+                                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',
+                                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',
+                                                ]) !!}
+                                            </div>
+                                        </div>
+                                    </x-jet-label>
+                                </div>
+                            @endif
+
+                            
+                            <div class="form-group text-right">
+                                <a class="float-left mt-3" href="{{ route('login') }}">
+                                    {{ __('Already registered?') }}
+                                </a>
+
+                                <x-jet-button class="btn btn-primary btn-lg btn-icon icon-right">
+                                    {{ __('Register in') }}
+                                </x-jet-button>
+                            </div>
+
+                        </form>
+
+                        <div class="text-center mt-5 text-small">
+                            Copyright &copy; Your Company. Made with 💙 by Stisla
+                            <div class="mt-2">
+                                <a href="#">Privacy Policy</a>
+                                <div class="bullet"></div>
+                                <a href="#">Terms of Service</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom"
+                    data-background="https://images.unsplash.com/photo-1539367628448-4bc5c9d171c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80">
+                    <div class="absolute-bottom-left index-2">
+                        <div class="text-light p-5 pb-2">
+                            <div class="mb-5 pb-3">
+                                <h1 class="mb-2 display-4 font-weight-bold">Good Morning</h1>
+                                <h5 class="font-weight-normal text-muted-transparent">Bali, Indonesia</h5>
+                            </div>
+                            Photo by <a class="text-light bb" target="_blank"
+                                href="https://unsplash.com/photos/Bi_5VsaOLnI">
+                                Alec Favale</a> on <a
+                                class="text-light bb" target="_blank" href="https://unsplash.com">Unsplash</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <!-- General JS Scripts -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="{{ url('stisla/js/stisla.js') }}"></script>
+
+    <!-- JS Libraies -->
+
+    <!-- Template JS File -->
+    <script src="{{ url('stisla/js/scripts.js') }}"></script>
+    <script src="{{ url('stisla/js/custom.js') }}"></script>
+
+    <!-- Page Specific JS File -->
+</body>
+
+</html>
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -57,4 +197,4 @@
             </div>
         </form>
     </x-jet-authentication-card>
-</x-guest-layout>
+</x-guest-layout> --}}
