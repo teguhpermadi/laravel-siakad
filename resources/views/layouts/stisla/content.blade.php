701723e1238ddf1459de7c4a304e2a8eb76fb87e
teguhpermadi <tguh.permadi@gmail.com>
2021-06-09 09:54:00 +0700

    install template stisla dan master templating blade

---
+++ resources/views/layouts/stisla/content.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.stisla.master')
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Blank Page</h1>
+        </div>
+
+        <div class="section-body">
+        </div>
+    </section>
+@endsection
+
+@section('js')
+    {{-- custom js per page --}}
+@endsection
