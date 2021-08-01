965dc2bfed6693af8b92fcc44d76d6afe80760d3
teguhpermadi <tguh.permadi@gmail.com>
2021-08-02 04:11:39 +0700

    2 agustus

--- resources/views/layouts/stisla/navbar.blade.php
+++ resources/views/layouts/stisla/navbar.blade.php
@@ -193,32 +193,7 @@
           </div>
         </div>
       </li>
-      <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-        <img alt="image" src="../assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
-        <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div></a>
-        <div class="dropdown-menu dropdown-menu-right">
-          <div class="dropdown-title">Logged in 5 min ago</div>
-          <!-- Account Management -->
-          <a href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')" class="dropdown-item has-icon">
-            <i class="far fa-user"></i> Profile
-          </a>
-          <a href="features-activities.html" class="dropdown-item has-icon">
-            <i class="fas fa-bolt"></i> Activities
-          </a>
-          <a href="features-settings.html" class="dropdown-item has-icon">
-            <i class="fas fa-cog"></i> Settings
-          </a>
-          <div class="dropdown-divider"></div>
-          <!-- Authentication -->
-          <form method="POST" action="{{ route('logout') }}">
-            @csrf
-            <a class="dropdown-item has-icon text-danger" href="{{ route('logout') }}"
-            onclick="event.preventDefault();
-                   this.closest('form').submit();">
-              <i class="fas fa-sign-out-alt"></i> Logout
-            </a>
-        </form>
-        </div>
-      </li>
+      @livewire('navbar-menu')
+      
     </ul>
   </nav>
---
+++ resources/views/livewire/navbar-menu.blade.php
@@ -0,0 +1,30 @@
+<div>
+    {{-- Care about people's approval and you will be their prisoner. --}}
+    <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+        <img alt="{{ Auth::user()->name }}" src="{{ Auth::user()->profile_photo_url }}" class="rounded-circle mr-1">
+        <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user()->name }}</div></a>
+        <div class="dropdown-menu dropdown-menu-right">
+          <div class="dropdown-title">Logged in 5 min ago</div>
+          <a href="features-profile.html" class="dropdown-item has-icon">
+            <i class="far fa-user"></i> Profile
+          </a>
+          <a href="features-activities.html" class="dropdown-item has-icon">
+            <i class="fas fa-bolt"></i> Activities
+          </a>
+          <!-- Account Management -->
+          <a href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')" class="dropdown-item has-icon">
+            <i class="fas fa-cog"></i> Settings
+          </a>
+          <div class="dropdown-divider"></div>
+          <!-- Authentication -->
+          <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <a class="dropdown-item has-icon text-danger" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+                   this.closest('form').submit();">
+              <i class="fas fa-sign-out-alt"></i> Logout
+            </a>
+        </form>
+        </div>
+      </li>
+</div>
